a9f069f690abcce50a2c763d4c1fcb16ceaaa309
1114-06\USER <chang180@gmail.com>
2021-10-23 16:21:38 +0800

    開始eloquent教學

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -12,12 +12,12 @@
     {{-- section sidebar --}}
     @section('sidebar')
         <h3>主版內容</h3>
-        {{-- @show --}}
-        @yield('sidebar')
+    @show
+    {{-- @yield('sidebar') --}}
 
-        <div class="container">
-            @yield('content')
-        </div>
-    </body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
 
-    </html>
+</html>
